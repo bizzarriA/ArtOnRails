e48ccafa7b80f61f64df93adadb802bdeca42150
bizzarriA <alice.bizzarri@student.unife.it>
2020-11-30 09:18:21 +0100

    Fix test search artista

--- test/models/search_test.rb
+++ test/models/search_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SearchTest < ActiveSupport::TestCase
   test "search" do
     search = searches(:one)
-    result = Search.search(search)[0]
+    result = Search.search(search)
     opera = operas(:one)
     # byebug
     assert result.include?(opera)
